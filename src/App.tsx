5ff355840472248098ebe99ce0b2cc246f498dba
EduframeAI <Eduframebuilder@gmail.com>
2025-05-21 18:07:57 +0800

    Update text colors to black

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import { ThemeProvider } from './contexts/ThemeContext';
 import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard';
 import GenerateVideo from './pages/GenerateVideo';
@@ -14,26 +13,22 @@ import XPBar from './components/XPBar';
 function App() {
   return (
     <AuthProvider>
-      <ThemeProvider>
-        <Router>
-          <div className="min-h-screen bg-white dark:bg-black flex flex-col">
-            <XPBar />
-            <Navbar />
-            <main className="flex-grow pt-16">
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/generate" element={<GenerateVideo />} />
-                <Route path="/saved" element={<SavedVideos />} />
-                <Route path="/discover" element={<VideosFromOthers />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </Router>
-      </ThemeProvider>
+      <Router>
+        <div className="min-h-screen bg-white flex flex-col">
+          <XPBar />
+          <Navbar />
+          <main className="flex-grow pt-16">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/generate" element={<GenerateVideo />} />
+              <Route path="/saved" element={<SavedVideos />} />
+              <Route path="/discover" element={<VideosFromOthers />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
     </AuthProvider>
   );
 }
-
-export default App
